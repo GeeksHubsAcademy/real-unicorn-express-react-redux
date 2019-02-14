c45fb1605bb2a0d59c1e417ebab8d9204a6548f9
Juan <juan@geekshubs.com>
2019-02-14 09:39:29 +0100

    city route and model created

--- backend/index.js
+++ backend/index.js
@@ -8,5 +8,6 @@ app.use(bodyParser.json());
 app.use('/team', require('./routes/team'));
 app.use('/user', require('./routes/user'));
 app.use('/role', require('./routes/role'));
+app.use('/city', require('./routes/city'));
 
 app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`))
